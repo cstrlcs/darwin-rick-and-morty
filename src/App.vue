f3f3d7ab62081a7fe3c9d5a0234ddd89a3ff52f0
Lucas Castro <45610153+cstrlcs@users.noreply.github.com>
2024-05-17 20:46:41 -0300

    feat: add fonts & transitions

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,23 @@ import Header from '@/components/ui/Header.vue'
   <main class="relative w-full min-h-screen p-16 pb-24 bg-gray-100">
     <div class="max-w-3xl mx-auto">
       <Header />
-      <RouterView />
+
+      <RouterView v-slot="{ Component }">
+        <Transition mode="out-in">
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
     </div>
   </main>
 </template>
+
+<style>
+.v-enter-active, .v-leave-active {
+  transition: opacity 0.1s ease-in-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
